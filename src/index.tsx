c177907af3e1f8fc183305c8a8c081ff854fbe9d
Xeilon <simmojoh98@gmail.com>
2021-11-16 03:18:40 -0700

    initial

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const Application = () => {
+    return <div>
+        <App />
+    </div>
+};
+
+ReactDOM.render(
+    <Application />,
+    document.querySelector('#root')
+)
